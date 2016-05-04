044080e085e12c372da6910e7100bedd7619cf39
Brian K <brianbrifri@gmail.com>
2016-05-04 18:54:51 -0500

    added random word generator

---
+++ app/src/main/java/edu/umsl/briankoehler/hangman/GameActivity.java
@@ -0,0 +1,10 @@
+package edu.umsl.briankoehler.hangman;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by b-kizzle on 5/4/16.
+ */
+public class GameActivity extends AppCompatActivity{
+
+}
--- app/src/main/java/edu/umsl/briankoehler/hangman/TitleScreenFragment.java
+++ app/src/main/java/edu/umsl/briankoehler/hangman/TitleScreenFragment.java
@@ -1,20 +1,26 @@
 package edu.umsl.briankoehler.hangman;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Brian Koehler on 4/25/2016.
  */
 public class TitleScreenFragment extends Fragment{
-    @Nullable
+    private Button mEasyButton;
+    private Button mMediumButton;
+    private Button mHardButton;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_screen_title_layout, container, false);
+        mEasyButton = (Button) view.findViewById(R.id.easy_button);
+        mMediumButton = (Button) view.findViewById(R.id.medium_button);
+        mHardButton = (Button) view.findViewById(R.id.hard_button);
         return view;
     }
 }
